40da1580e8d350ee395ee4cf902c18c75604ab7b
matrixx1211 <bitomsky.marek121100@gmail.com>
2022-04-23 21:50:58 +0200

    done s responzivitou

--- script/navigation.js
+++ script/navigation.js
@@ -1,9 +1,13 @@
-/* Otevře navigaci */
-function openNav() {
-	document.getElementById("mySidenav").style.width = "100%";
-  }
-  
-  /* Close/hide the sidenav */
-  function closeNav() {
-	document.getElementById("mySidenav").style.width = "0";
-  }
+/* Otevře navigaci v režimu pro telefony/tablety*/
+function toggle_responsive() {
+	if (window.innerWidth <= 1400) {
+		var nav_element = document.getElementById("nav-content");
+		if (nav_element.className === "nav-list") {
+			nav_element.className += " responsive";
+			nav_element.style.display = "block";
+		} else {
+			nav_element.className = "nav-list";
+			nav_element.style.display = "none";
+		}
+	}
+}
