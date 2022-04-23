3cf0e484f923116ae1c460bb11e3a348c2633c94
matrixx1211 <bitomsky.marek121100@gmail.com>
2022-04-23 18:33:29 +0200

    done bez responzivity

--- script/scrolling.js
+++ script/scrolling.js
@@ -1,44 +1,15 @@
-$('.nav li').find('a[href^="#"]').click(function(event) { 
-    // Prevent from default action to intitiate
-    event.preventDefault();
-
-    $('.nav li a').removeClass("active");
-    $(this).addClass("active");
-
-
-    // The id of the section we want to go to.
-    var anchorId = $(this).attr("href");
-    
-    // Our scroll target : the top position of the
-    // section that has the id referenced by our href.
-    var target = $(anchorId).offset().top - offset;
-    //console.log(target);
-    
-    
-    $('html, body').animate({ scrollTop: target }, 500, function () {
-        //window.location.hash = '!' + id;
-        window.location.hash = anchorId;        
-    });
-    
-
-});
-
-function setActiveListElements(event){
-    // Get the offset of the window from the top of page
-    var windowPos = $(window).scrollTop();
-    $('.nav li a[href^="#"]').each(function() { 
-		var anchorId = $(this);
-        var target = $(anchorId.attr("href"));
+/* Skript na skrolování */
+$(window).scroll(function() {
+    var window_position = $(window).scrollTop();
+    $('.nav a[href^="#"]').each(function() { 
+		var nav_item = $(this);
+        var target = $(nav_item.attr("href"));
         
         if (target.length > 0) {
-            if (target.position().top <= windowPos && target.position().top + target.height() > windowPos) {
-                $('.nav li a').removeClass("active");
-                anchorId.addClass("active");
+            if (target.position().top + target.height() > window_position && target.position().top-10 <= window_position) {
+                $('.nav a').removeClass("active");
+                nav_item.addClass("active");
             }
         }
     });
-}
-
-$(window).scroll(function() {
-    setActiveListElements();
 });
