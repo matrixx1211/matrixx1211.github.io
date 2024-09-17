15a2ba97aed434d7ff9f10fbb9d1dc7ea696f09c
Marek Bitomský <55531170+matrixx1211@users.noreply.github.com>
2024-09-18 00:27:35 +0200

    snad poslední větší desktop deploy

---
+++ assets/PageNumber-CoFomNsS.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-BzgU2gYV.js";import{m as o}from"./motion-B4hoxqpt.js";function t({number:s,anim:r,style:e}){return a.jsx(o.div,{className:e.pageIndicator,...r?r.pageIndicator:{},children:a.jsx(o.span,{className:e.pageNumber,...r?r.pageNumber:{},children:s})})}export{t as P};
