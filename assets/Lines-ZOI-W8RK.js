217b5fcab3b5892e12cb6048ca519175f375e533
Marek Bitomský <55531170+matrixx1211@users.noreply.github.com>
2025-03-15 03:55:05 +0100

    Úplně překopaná verze

---
+++ assets/Lines-ZOI-W8RK.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-D4vIppqs.js";import{a}from"./Line-7U_fYSrF.js";function j({lines:n,className:t="",textAlign:i="center",plusDelay:s=0,prefix:o}){return e.jsx("div",{className:t,children:n.map((m,r)=>e.jsx(a,{textAlign:i,index:r,plusDelay:s,children:m},`${o}-${r}`))})}export{j as L};
