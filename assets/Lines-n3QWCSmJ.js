02eee0affdd8bc658f398e94e3c12c51262cc20d
Marek Bitomský <bitomsky.marek121100@gmail.com>
2025-03-31 01:25:43 +0200

    new version to test

---
+++ assets/Lines-n3QWCSmJ.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DXhAUj-p.js";import{a}from"./Line-CTDQvpR-.js";function j({lines:n,className:t="",textAlign:i="center",plusDelay:s=0,prefix:o}){return e.jsx("div",{className:t,children:n.map((m,r)=>e.jsx(a,{textAlign:i,index:r,plusDelay:s,children:m},`${o}-${r}`))})}export{j as L};
